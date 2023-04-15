887a954cdd0fd1832a34210c291ba48d3b187a60
Muhammet933321 <78547346+Muhammet933321@users.noreply.github.com>
2023-04-15 18:07:24 +0300

    LevelSystemUpgrading

---
+++ Assets/scripts/MaınMenuSystem/MaınMEnu.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaınMEnu : MonoBehaviour
+{
+
+    void MultiplayerGame()
+    {
+
+
+    }
+
+    void OnlineGame()
+    {
+
+
+    }
+
+    void PlayTwoPlayer()
+    {
+
+
+    }
+
+}
